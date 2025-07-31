78ce67805f3fa8c52b391e46fc505181b4ef89be
jlembeck06 <jonatan.lembeck@philips.com>
2025-07-31 16:33:07 -0300

    feat(added postgreSQL)

---
+++ src/db.js
@@ -0,0 +1,5 @@
+const { Sequelize, Model} = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DB_URL, {dialect: 'postgres', });
+
+module.exports = sequelize;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('./db');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -8,6 +9,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.get('/', (req, res) => {
+    res.send('Welcome to the Config Store API');
 });
+
+db.authenticate()
+    .then(() => {
+        console.log('Connected to PostgreSQL');
+        return db.sync();
+    })
+    .then(() => {
+        console.log('Database synchronized, starting server.');
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Unable to connect to DB');
+        console.error(err);
+    });
